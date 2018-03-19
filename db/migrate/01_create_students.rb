05b42a85c23aef37cdd09e589173f9aa4347c710
adam n <adam.nawroc@gmail.com>
2018-03-19 15:41:35 +0000

    automatically backed up by learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -2,9 +2,9 @@ class CreateStudents < ActiveRecord::Migration[5.1]
   def change
     create_table :students do |t|
       t.string :name
-      t.string :genre
-      t.integer :age
-      t.string :hometown
+      # t.string :genre
+      # t.integer :age
+      # t.string :hometown
     end
   end
 end

9b71d7fec121d146f5bf789664a1e7d421b441f0
adam n <adam.nawroc@gmail.com>
2018-03-19 15:44:08 +0000

    automatically backed up by learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,7 +1,7 @@
 class AddGradeAndBirthdateToStudents < ActiveRecord::Migration
   def change
-      add_column :students, :favorite_food, :string
-      add_column :students, :favorite_food, :string
+      add_column :students, :grade, :string
+      add_column :students, :birthdate, :string
     end
 
 end
